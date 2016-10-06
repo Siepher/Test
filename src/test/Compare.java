69be78fcc354542fa391674347059097b11e71fa
student <student@172.26.0.104>
2016-10-06 11:26:26 -0600

    Help

---
+++ src/test/Compare.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author student
+ */
+
+    /**
+     * @param args the command line arguments
+     */
+       import java.util.Scanner;
+
+public class Compare {
+    public static void main(String[] args) { 
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if(number == int); 
+            System.out.println("Number is natural and positive.");
+    }
+}
+
+    
+
+    
+
