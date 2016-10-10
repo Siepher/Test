12df6c434746a17702d13229482730dfb863d568
student <student@172.26.3.174>
2016-10-10 14:16:30 -0600

    Better

---
+++ Test2/src/test/Test.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author student
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+            System.out.println("Enter number: ");
+                      
+                int n = input.nextInt();
+                              
+                              
+               if (n < 0) {
+    // negative 
+               
+
+                } else {
+                   double root = Math.sqrt(n);
+                   double absolute = Math.abs(n); 
+                           System.out.print(root);
+
+                   
+   // it's a positive
+               }        
+                
+            
+                
+
+
+    
+    }
+    
+}
